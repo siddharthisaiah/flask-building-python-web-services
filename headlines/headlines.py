a43d3806d2ff72a412f1e17d6ebf9a0a49e7240d
Siddharth <siddharthisaiah@gmail.com>
2017-11-27 13:00:46 +0530

    end of chapter 3

---
+++ headlines/headlines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env
+
+
+import feedparser
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+RSS_FEEDS = {
+                'bbc': "http://feeds.bbci.co.uk/news/rss.xml",
+                'cnn': 'http://rss.cnn.com/rss/edition.rss%2520',
+                'fox': 'http://feeds.foxnews.com/foxnews/latest',
+                'iol': 'http://rss.iol.io/iol/all-content-feed'
+            }
+
+
+@app.route('/')
+@app.route('/<publication>')
+def get_news(publication='bbc'):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    return render_template("home.html", articles=feed['entries'])
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
