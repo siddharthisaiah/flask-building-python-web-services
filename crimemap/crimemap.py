94bac02b254c76167833a981bd21da3dcf96819e
Siddharth <siddharthisaiah@gmail.com>
2017-12-08 14:38:52 +0530

    complete ch.8 date error css not working

--- crimemap/crimemap.py
+++ crimemap/crimemap.py
@@ -1,22 +1,27 @@
+import dateparser
+import datetime
 from flask import Flask
 import json
 from flask import render_template
 from flask import request
 import dbconfig
+import string
 if dbconfig.test:
     from mockdbhelper import MockDBHelper as DBHelper
 else:
     from dbhelper import DBHelper as DBHelper
 
+
 app = Flask(__name__)
 DB = DBHelper()
+categories = ['mugging', 'break-in']
 
 
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes, categories=categories, error_message=error_message)
 
 
 @app.route("/add", methods=['POST'])
@@ -41,12 +46,32 @@ def clear():
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return home()
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
+
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+
+def sanitize_string(userinput):
+    whitelist = string.ascii_letters + string.digits + " !?$.,;:-'()&"
+    return ''.join([x for x in userinput if x in whitelist])
 
 
 if __name__ == '__main__':
