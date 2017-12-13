3404ea7d0bf83f80c45be41fd41d0d66767c0e9a
Siddharth <siddharthisaiah@gmail.com>
2017-12-13 17:17:55 +0530

    complete password hashing and register functionality

---
+++ waitercaller/passwordhelper.py
@@ -0,0 +1,15 @@
+import base64
+import hashlib
+import os
+
+
+class PasswordHelper:
+
+    def get_hash(self, plain):
+        return hashlib.sha512(plain.encode()).hexdigest()
+
+    def get_salt(self):
+        return base64.b64encode(os.urandom(20)).decode()
+
+    def validate_password(self, plain, salt, expected):
+        return self.get_hash(plain+salt) == expected
--- waitercaller/waitercaller.py
+++ waitercaller/waitercaller.py
@@ -8,10 +8,13 @@ from flask_login import login_required
 from flask_login import login_user
 from flask_login import logout_user
 from mockdbhelper import MockDBHelper as DBHelper
+from passwordhelper import PasswordHelper
 from user import User
 
 
 DB = DBHelper()
+PH = PasswordHelper()
+
 app = Flask(__name__)
 app.secret_key = 't2CNVBVfXZw3viLkBe8tOkgABDhaFsnpo/WE8xdzDOK9S2wIgNeYbeyDGR177c+MR3AyQakKuNSLJGWdcqaH/31F83GOHDz0mxR'
 login_manager = LoginManager(app)
@@ -32,8 +35,8 @@ def account():
 def login():
     email = request.form.get("email")
     password = request.form.get("password")
-    user_password = DB.get_user(email)
-    if user_password and user_password == password:
+    stored_user = DB.get_user(email)
+    if stored_user and PH.validate_password(password, stored_user['salt'], stored_user['hashed']):
         user = User(email)
         login_user(user, remember=True)
         return redirect(url_for('account'))
@@ -50,6 +53,21 @@ def load_user(user_id):
 @app.route("/logout")
 def logout():
     logout_user()
+    return redirect(url_for("home"))
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    email = request.form.get("email")
+    pw1 = request.form.get("password")
+    pw2 = request.form.get("password2")
+    if not pw1 == pw2:
+        return redirect(url_for("home"))
+    if DB.get_user(email):
+        return redirect(url_for("home"))
+    salt = PH.get_salt()
+    hashed = PH.get_hash(pw1 + salt)
+    DB.add_user(email, salt, hashed)
     return redirect(url_for("home"))
 
 
