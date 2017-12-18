a96b3d5297fc9556e96ae0a33d2c6e4b2dadb6db
Siddharth <siddharthisaiah@gmail.com>
2017-12-18 14:12:24 +0530

    add functionality to handle attention requests

--- waitercaller/mockdbhelper.py
+++ waitercaller/mockdbhelper.py
@@ -1,3 +1,7 @@
+import datetime
+
+MOCK_REQUESTS = [{"_id": "1", "table_number": "1", "table_id": "1", "time": datetime.datetime.now()}]
+
 MOCK_TABLES = [{"_id": 1, "number": 1, "owner": "test@example.com", "url": "mockurl"}]
 
 MOCK_USERS = [{"email": "test@example.com", "salt": "8Fb23mMNHD5Zb8pr2qWA3PE9bH0=",
@@ -26,8 +30,28 @@ class MockDBHelper:
     def get_tables(self, owner_id):
         return MOCK_TABLES
 
+    def get_table(self, table_id):
+        for table in MOCK_TABLES:
+            if table.get("_id") == table_id:
+                return table
+
     def delete_table(self, table_id):
         for i, table in enumerate(MOCK_TABLES):
             if table.get("_id") == table_id:
                 del MOCK_TABLES[i]
+                break
+
+    def add_request(self, table_id, time):
+        table = self.get_table(table_id)
+        MOCK_REQUESTS.append({"_id": table_id, "owner": table[
+            "owner"], "table_number": table["number"], "table_id": table_id, "time": time})
+        return True
+
+    def get_requests(self, owner_id):
+        return MOCK_REQUESTS
+
+    def delete_request(self, request_id):
+        for i, request in enumerate(MOCK_REQUESTS):
+            if request.get("_id") == request_id:
+                del MOCK_REQUESTS[i]
                 break
--- waitercaller/waitercaller.py
+++ waitercaller/waitercaller.py
@@ -1,14 +1,19 @@
 import config
+
+import datetime
+
 from flask import Flask
 from flask import render_template
 from flask import redirect
 from flask import request
 from flask import url_for
+
 from flask_login import current_user
 from flask_login import LoginManager
 from flask_login import login_required
 from flask_login import login_user
 from flask_login import logout_user
+
 from bitlyhelper import BitlyHelper
 from mockdbhelper import MockDBHelper as DBHelper
 from passwordhelper import PasswordHelper
@@ -79,7 +84,12 @@ def register():
 @app.route("/dashboard")
 @login_required
 def dashboard():
-    return render_template("dashboard.html")
+    now = datetime.datetime.now()
+    requests = DB.get_requests(current_user.get_id())
+    for req in requests:
+        deltaseconds = (now - req['time']).seconds
+        req['wait_minutes'] = "{}.{}".format((deltaseconds/60), str(deltaseconds % 60).zfill(2))
+    return render_template("dashboard.html", requests=requests)
 
 
 @app.route("/account/createtable", methods=["POST"])
@@ -98,6 +108,20 @@ def account_deletetable():
     tableid = request.args.get("tableid")
     DB.delete_table(tableid)
     return redirect(url_for('account'))
+
+
+@app.route("/newrequest/<tid>")
+def new_request(tid):
+    DB.add_request(tid, datetime.datetime.now())
+    return "Your request has been logged and a waiter will be with you shortly"
+
+
+@app.route("/dashboard/resolve")
+@login_required
+def dashboard_resolve():
+    request_id = request.args.get("request_id")
+    DB.delete_request(request_id)
+    return redirect(url_for('dashboard'))
 
 
 if __name__ == '__main__':
